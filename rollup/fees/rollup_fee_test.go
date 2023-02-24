5599050d1526b11a96691de3d64e7f610d359132
HAOYUatHZ <haoyu@protonmail.com>
2023-02-24 08:03:29 +0800

    add `TestCalculateL1Fee`

--- rollup/fees/rollup_fee_test.go
+++ rollup/fees/rollup_fee_test.go
@@ -3,9 +3,18 @@ package fees
 import (
 	"math/big"
 	"testing"
+
+	"github.com/stretchr/testify/assert"
 )
 
-func TestMulAndScale(t *testing.T) {
-	a := new(big.Int)
-	testing.Log(a)
+func TestCalculateL1Fee(t *testing.T) {
+	l1BaseFee := new(big.Int).SetUint64(15000000)
+
+	data := []byte{0, 10, 1, 0}
+	overhead := new(big.Int).SetUint64(100)
+	scalar := new(big.Int).SetUint64(1e8)
+
+	expected := new(big.Int).SetUint64(1842000000)
+	actual := CalculateL1Fee(data, overhead, l1BaseFee, scalar)
+	assert.Equal(t, expected, actual)
 }

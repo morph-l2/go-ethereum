ee3a0b794af81e199ca6736e5b1ff561396274e2
Max Wolff <maxcwolff@gmail.com>
2023-03-30 23:37:51 -0700

    goimports

--- core/types/receipt.go
+++ core/types/receipt.go
@@ -86,7 +86,7 @@ type receiptMarshaling struct {
 	GasUsed           hexutil.Uint64
 	BlockNumber       *hexutil.Big
 	TransactionIndex  hexutil.Uint
-	L1Fee 		      *hexutil.Big
+	L1Fee             *hexutil.Big
 }
 
 // receiptRLP is the consensus encoding of a receipt.

6030467fee2d5ada43c9f7969e7d7c410f48778f
HAOYUatHZ <haoyu@protonmail.com>
2023-05-12 10:00:27 +0800

    finish rust codes

--- rollup/circuitscapacitychecker/libzkp/libzkp.h
+++ rollup/circuitscapacitychecker/libzkp/libzkp.h
@@ -1,2 +1,3 @@
 new_circuit_capacity_checker();
 reset_circuit_capacity_checker();
+int apply_tx(char *tx_traces);
--- rollup/circuitscapacitychecker/libzkp/src/lib.rs
+++ rollup/circuitscapacitychecker/libzkp/src/lib.rs
@@ -1,13 +1,11 @@
 #![feature(once_cell)]
 
 pub mod checker {
-    use crate::utils::{c_char_to_str, c_char_to_vec, vec_to_c_char};
+    use crate::utils::{c_char_to_vec, bool_to_int};
     use libc::c_char;
     use std::cell::OnceCell;
     use std::panic;
-    use std::ptr::null;
     use types::eth::BlockTrace;
-    use zkevm::circuit::AGG_DEGREE;
     use zkevm::{capacity_checker::CircuitCapacityChecker};
 
     static mut CHECKER: OnceCell<CircuitCapacityChecker> = OnceCell::new();
@@ -27,6 +25,18 @@ pub mod checker {
     pub unsafe extern "C" fn reset_circuit_capacity_checker() {
         CHECKER.get_mut().unwrap().reset()
     }
+
+    /// # Safety
+    #[no_mangle]
+    pub unsafe extern "C" fn apply_tx(tx_traces: *const c_char) -> u8 {
+        let tx_traces_vec = c_char_to_vec(tx_traces);
+        let traces = serde_json::from_slice::<Vec<BlockTrace>>(&tx_traces_vec).unwrap();
+        let ok = panic::catch_unwind(|| {
+            CHECKER.get_mut().unwrap()
+                .estimate_circuit_capacity(traces.as_slice()).is_ok()
+        });
+        ok.map_or(0, bool_to_int)
+    }
 }
 
 pub(crate) mod utils {
@@ -45,5 +55,12 @@ pub(crate) mod utils {
 
     pub(crate) fn vec_to_c_char(bytes: Vec<u8>) -> *const c_char {
         CString::new(bytes).unwrap().into_raw()
+    }
+
+    pub(crate) fn bool_to_int(b: bool) -> u8 {
+        match b {
+            true => 1,
+            false => 0,
+        }
     }
 }

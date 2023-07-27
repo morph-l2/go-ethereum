00127a3aac36b97d67eaf51aacfe5bc0635ca3db
Yu Marvel <marvel.yu@bitget.com>
2023-07-27 18:05:04 +0800

    update run testnet shell

--- run-testnet.sh
+++ run-testnet.sh
@@ -1,16 +1,9 @@
-GETH_DATA_DIR="${GETH_DATA_DIR:-/db}"
+GETH_DATA_DIR="${GETH_DATA_DIR:-/data/morphism/setup/morph-geth-data}"
 GETH_CHAINDATA_DIR="$GETH_DATA_DIR/geth/chaindata"
-GENESIS_FILE_PATH="${GENESIS_FILE_PATH:-/genesis.json}"
-JWT_SECRET_PATH="${JWT_SECRET_PATH:-/jwt-secret.txt}"
+JWT_SECRET_PATH="${JWT_SECRET_PATH:-/data/morphism/setup/jwt-secret.txt}"
 DEFAULE_MINER_ETHERBASE="0x0e87cd091e091562F25CB1cf4641065dA2C049F5"
+CHAIN_ID="${CHAIN_ID:-2710}"
 
-if [[ ! -e "$GETH_CHAINDATA_DIR" ]]; then
-  echo "$GETH_CHAINDATA_DIR missing, running init"
-  echo "Initializing genesis."
-  geth --verbosity=3 init --datadir="$GETH_DATA_DIR"  "$GENESIS_FILE_PATH"
-else
-	echo "$GETH_KEYSTORE_DIR exists."
-fi
 
 if [[ -z "$MINER_ETHERBASE" ]]; then
   # the environment variable is missing, set a default value
@@ -28,8 +21,13 @@ COMMAND="geth \
 --http.vhosts="*" \
 --http.addr=0.0.0.0 \
 --http.port=8545 \
---http.api=web3,debug,eth,txpool,net,scroll,engine,admin \
---networkid=53077 \
+--http.api=web3,eth,txpool,net,scroll,engine,admin \
+--ws \
+--ws.addr=0.0.0.0 \
+--ws.port=8546 \
+--ws.origins="*" \
+--ws.api=web3,eth,txpool,net,scroll,engine,admin \
+--networkid=$CHAIN_ID \
 --authrpc.addr="0.0.0.0" \
 --authrpc.port="8551" \
 --authrpc.vhosts="*" \

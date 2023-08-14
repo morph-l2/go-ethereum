af70401f9d34052b0717b21770f7c8d848ebd4bc
fletcher142 <fanciture@163.com>
2023-08-14 20:31:33 +0800

    add admin for sentry

--- run-testnet-sentry.sh
+++ run-testnet-sentry.sh
@@ -20,7 +20,7 @@ COMMAND="geth \
 --http.vhosts="*" \
 --http.addr=0.0.0.0 \
 --http.port=8545 \
---http.api=web3,eth,txpool,net,engine \
+--http.api=web3,eth,txpool,net,admin,engine \
 --ws \
 --ws.addr=0.0.0.0 \
 --ws.port=8546 \

5c6914cab36c31512f67206555f51089e52f8d15
FletcherMan <fanciture@163.com>
2023-08-25 21:32:05 +0800

    put dirties in cache and move to database when commit

--- trie/zk_trie_database.go
+++ trie/zk_trie_database.go
@@ -15,22 +15,33 @@ import (
 type ZktrieDatabase struct {
 	db     *Database
 	prefix []byte
+
+	rawDirtiesCache KvMap
 }
 
 func NewZktrieDatabase(diskdb ethdb.KeyValueStore) *ZktrieDatabase {
-	return &ZktrieDatabase{db: NewDatabase(diskdb), prefix: []byte{}}
+	return &ZktrieDatabase{db: NewDatabase(diskdb), prefix: []byte{}, rawDirtiesCache: make(KvMap)}
 }
 
 // adhoc wrapper...
 func NewZktrieDatabaseFromTriedb(db *Database) *ZktrieDatabase {
 	db.Zktrie = true
-	return &ZktrieDatabase{db: db, prefix: []byte{}}
+	return &ZktrieDatabase{db: db, prefix: []byte{}, rawDirtiesCache: make(KvMap)}
 }
 
-// Put saves a key:value into the Storage
+// Commit commits the cached data into the Storage
+func (l *ZktrieDatabase) Commit() {
+	l.db.lock.Lock()
+	defer l.db.lock.Unlock()
+	if l.db.dirties != nil {
+		l.db.rawDirties.AddFrom(l.rawDirtiesCache)
+	}
+}
+
+// Put saves a key:value into the Cache
 func (l *ZktrieDatabase) Put(k, v []byte) error {
 	l.db.lock.Lock()
-	l.db.rawDirties.Put(Concat(l.prefix, k[:]), v)
+	l.rawDirtiesCache.Put(Concat(l.prefix, k[:]), v)
 	l.db.lock.Unlock()
 	return nil
 }
@@ -39,7 +50,10 @@ func (l *ZktrieDatabase) Put(k, v []byte) error {
 func (l *ZktrieDatabase) Get(key []byte) ([]byte, error) {
 	concatKey := Concat(l.prefix, key[:])
 	l.db.lock.RLock()
-	value, ok := l.db.rawDirties.Get(concatKey)
+	value, ok := l.rawDirtiesCache.Get(concatKey)
+	if !ok {
+		value, ok = l.db.rawDirties.Get(concatKey)
+	}
 	l.db.lock.RUnlock()
 	if ok {
 		return value, nil

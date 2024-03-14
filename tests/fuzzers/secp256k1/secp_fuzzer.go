8b0a8e1d2dedc936dec0eb479690b0d7047c89c3
fletcher.fan <fletcher.fan@fletcher2fan.local>
2024-03-14 15:24:44 +0800

    use btcec/v2 & bump crypto version to v0.21.0

--- tests/fuzzers/secp256k1/secp_fuzzer.go
+++ tests/fuzzers/secp256k1/secp_fuzzer.go
@@ -21,7 +21,8 @@ package secp256k1
 import (
 	"fmt"
 
-	"github.com/btcsuite/btcd/btcec"
+	"github.com/btcsuite/btcd/btcec/v2"
+
 	fuzz "github.com/google/gofuzz"
 
 	"github.com/scroll-tech/go-ethereum/crypto/secp256k1"

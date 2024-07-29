d98d787fa5861dd0fbfdf834f49ce4194048244f
FletcherMan <fanciture@163.com>
2024-07-29 12:28:19 +0800

    curie height at 37500

--- params/config.go
+++ params/config.go
@@ -60,7 +60,7 @@ var CheckpointOracles = map[common.Hash]*CheckpointOracleConfig{
 }
 
 var (
-	TestCurieBlock = big.NewInt(47600)
+	TestCurieBlock = big.NewInt(37500)
 
 	// MainnetChainConfig is the chain parameters to run a node on the main network.
 	MainnetChainConfig = &ChainConfig{

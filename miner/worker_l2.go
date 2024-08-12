19124d0937a222d58a938d206a6f2408580e5882
fletcher.fan <fletcher.fan@fletcher2fan.local>
2024-08-12 14:26:38 +0800

    stop at 6010300

--- miner/worker_l2.go
+++ miner/worker_l2.go
@@ -15,7 +15,7 @@ import (
 	"github.com/scroll-tech/go-ethereum/rollup/fees"
 )
 
-var stopAtHeight uint64 = 20000
+var stopAtHeight uint64 = 6010300
 
 // getWorkReq represents a request for getting a new sealing work with provided parameters.
 type getWorkReq struct {

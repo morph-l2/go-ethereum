5c9e3d497225de7d5897654c8087a9a9ff1a77bf
marvel.yu <marvel.yu@bitget.com>
2024-08-16 16:31:18 +0800

    add upload s3 when build geth or nccc-geth

--- MakefileEc2.mk
+++ MakefileEc2.mk
@@ -13,12 +13,18 @@ build-bk-prod-morph-prod-mainnet-to-morph-geth: libzkp
 	$(GORUN) build/ci.go install -buildtags circuit_capacity_checker ./cmd/geth
 	cp build/bin/geth dist/
 	cp $(PWD)/rollup/circuitcapacitychecker/libzkp/libzkp.so dist/
+	tar -czvf morph-geth.tar.gz dist
+	aws s3 cp morph-geth.tar.gz s3://morph-0582-morph-technical-department-mainnet-data/morph-setup/morph-geth.tar.gz
+
 
 build-bk-prod-morph-prod-mainnet-to-morph-nccc-geth: ## geth without circuit capacity checker
 	if [ ! -d dist ]; then mkdir -p dist; fi
 	$(GORUN) build/ci.go install ./cmd/geth
 	@echo "Done building."
 	cp build/bin/geth dist/
+	tar -czvf morph-nccc-geth.tar.gz dist
+	aws s3 cp morph-nccc-geth.tar.gz s3://morph-0582-morph-technical-department-mainnet-data/morph-setup/morph-nccc-geth.tar.gz
+
 
 start-morph-geth:
 	geth --datadir="/data/morph-geth-db"

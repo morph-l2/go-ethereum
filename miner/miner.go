3bb1e086c625b0777429f8c04948c9c1f2023a1a
ryanmorphl2 <163962984+ryanmorphl2@users.noreply.github.com>
2024-09-26 11:47:21 +0800

    feat(miner):set miner default gasPrice to 0

--- miner/miner.go
+++ miner/miner.go
@@ -63,7 +63,7 @@ type Config struct {
 // DefaultConfig contains default settings for miner.
 var DefaultConfig = Config{
 	GasCeil:  8000000,
-	GasPrice: big.NewInt(params.GWei / 1000),
+	GasPrice: big.NewInt(0),
 
 	// The default recommit time is chosen as two seconds since
 	// consensus-layer usually will wait a half slot of time(6s)

566a2dd0c9f069e22c3789fef743f408cd191f90
FletcherMan <fanciture@163.com>
2024-10-14 14:20:59 +0800

    recover maxTxPerBlock

--- params/config.go
+++ params/config.go
@@ -255,7 +255,7 @@ var (
 	}
 
 	MorphMaxTxPayloadBytesPerBlock = 120 * 1024
-	MorphMaxTxPerBlock             = 1000
+	MorphMaxTxPerBlock             = 100
 
 	MorphFeeVaultAddress    = common.HexToAddress("0x48442aa154897eef141df231cc1517fc8c1d170f")
 	MorphHoleskyChainConfig = &ChainConfig{

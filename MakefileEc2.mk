570a56654831b8c9a8adfc97249d2ca814d31b23
ryanmorphl2 <163962984+ryanmorphl2@users.noreply.github.com>
2024-12-24 11:54:49 +0800

    merge main

--- MakefileEc2.mk
+++ MakefileEc2.mk
@@ -21,3 +21,34 @@ build-bk-prod-morph-prod-mainnet-to-morph-nccc-geth:
 	aws s3 cp morph-nccc-geth.tar.gz s3://morph-0582-morph-technical-department-mainnet-data/morph-setup/morph-nccc-geth.tar.gz
 
 
+# build for holesky
+build-bk-prod-morph-prod-testnet-to-morph-geth-holesky:
+	if [ ! -d dist ]; then mkdir -p dist; fi
+	$(GORUN) build/ci.go install ./cmd/geth
+	cp build/bin/geth dist/
+	tar -czvf morph-geth.tar.gz dist
+	aws s3 cp morph-geth.tar.gz s3://morph-0582-morph-technical-department-testnet-data/testnet/holesky/morph-setup/morph-geth.tar.gz
+
+build-bk-prod-morph-prod-testnet-to-morph-nccc-geth-holesky:
+	if [ ! -d dist ]; then mkdir -p dist; fi
+	$(GORUN) build/ci.go install ./cmd/geth
+	@echo "Done building."
+	cp build/bin/geth dist/
+	tar -czvf morph-nccc-geth.tar.gz dist
+	aws s3 cp morph-nccc-geth.tar.gz s3://morph-0582-morph-technical-department-testnet-data/testnet/holesky/morph-setup/morph-nccc-geth.tar.gz
+
+build-bk-test-morph-test-qanet-to-morph-geth-qanet:
+	if [ ! -d dist ]; then mkdir -p dist; fi
+	$(GORUN) build/ci.go install ./cmd/geth
+	@echo "Done building."
+	cp build/bin/geth dist/
+	tar -czvf morph-geth.tar.gz dist
+	aws s3 cp morph-geth.tar.gz s3://morph-7637-morph-technical-department-qanet-data/morph-setup/morph-geth.tar.gz
+
+build-bk-test-morph-test-qanet-to-morph-nccc-geth-qanet:
+	if [ ! -d dist ]; then mkdir -p dist; fi
+	$(GORUN) build/ci.go install ./cmd/geth
+	@echo "Done building."
+	cp build/bin/geth dist/
+	tar -czvf morph-nccc-geth.tar.gz dist
+	aws s3 cp morph-nccc-geth.tar.gz s3://morph-7637-morph-technical-department-qanet-data/morph-setup/morph-nccc-geth.tar.gz

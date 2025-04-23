5dbe4057d8118d788130585f6a026201944a2921
ryanmorphl2 <163962984+ryanmorphl2@users.noreply.github.com>
2025-04-23 13:43:48 +0800

    set max dirtyBufferSize

--- triedb/pathdb/database.go
+++ triedb/pathdb/database.go
@@ -45,7 +45,7 @@ const (
 	// Too large nodebuffer will cause the system to pause for a long
 	// time when write happens. Also, the largest batch that pebble can
 	// support is 4GB, node will panic if batch size exceeds this limit.
-	MaxDirtyBufferSize = 64 * 1024 * 1024
+	MaxDirtyBufferSize = 128 * 1024 * 1024
 
 	// DefaultDirtyBufferSize is the default memory allowance of node buffer
 	// that aggregates the writes from above until it's flushed into the

7871870cb8eb12d5c2649ca08aef74140b6a5aaf
fletcher.fan <fletcher.fan@bitget.com>
2025-10-20 11:07:57 +0800

    update viridian time for hoodi and mainnet

--- params/config.go
+++ params/config.go
@@ -316,6 +316,7 @@ var (
 		BernoulliBlock:          big.NewInt(0),
 		CurieBlock:              big.NewInt(0),
 		Morph203Time:            NewUint64(0),
+		ViridianTime:            NewUint64(1761544800),
 		TerminalTotalDifficulty: big.NewInt(0),
 		Morph: MorphConfig{
 			UseZktrie:                 true,
@@ -346,6 +347,7 @@ var (
 		BernoulliBlock:          big.NewInt(0),
 		CurieBlock:              big.NewInt(0),
 		Morph203Time:            NewUint64(1747029600),
+		ViridianTime:            NewUint64(1762149600), // 2025-10-20 04:00:00 UTC
 		TerminalTotalDifficulty: big.NewInt(0),
 		Morph: MorphConfig{
 			UseZktrie:                 true,

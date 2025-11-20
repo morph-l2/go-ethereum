adf5a14135f663e7b122188ff8af02909dbc0417
corey <coreyx1992@gmail.com>
2025-11-20 15:26:20 +0800

    Revert changes (#251)
    revert changes
    
    Co-authored-by: corey <corey.zhang@bitget.com>

--- eth/gasprice/feehistory.go
+++ eth/gasprice/feehistory.go
@@ -120,8 +120,7 @@ func (oracle *Oracle) processBlock(bf *blockFees, percentiles []float64) {
 	}
 	sorter := make(sortGasAndReward, len(bf.block.Transactions()))
 	for i, tx := range bf.block.Transactions() {
-		reward := big.NewInt(0)
-		reward, _ = tx.EffectiveGasTip(bf.block.BaseFee())
+		reward, _ := tx.EffectiveGasTip(bf.block.BaseFee())
 		sorter[i] = txGasAndReward{gasUsed: bf.receipts[i].GasUsed, reward: reward}
 	}
 	sort.Sort(sorter)

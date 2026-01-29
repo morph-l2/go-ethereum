f0520fa4052b7a518813862b165bc749bf33305c
segue <huoda.china@gmail.com>
2026-01-30 04:49:36 +0800

    fix sth

--- core/types/morph_tx.go
+++ core/types/morph_tx.go
@@ -2,7 +2,9 @@ package types
 
 import (
 	"bytes"
+	"errors"
 	"math/big"
+	"strconv"
 
 	"github.com/morph-l2/go-ethereum/common"
 	"github.com/morph-l2/go-ethereum/rlp"
@@ -43,20 +45,21 @@ type MorphTx struct {
 	Data       []byte
 	AccessList AccessList
 
-	Version    uint8             // version of morph tx (0 = legacy, 1 = with reference/memo)
-	FeeTokenID uint16            // ERC20 token ID for fee payment (0 = ETH)
-	FeeLimit   *big.Int          // maximum fee in token units (optional)
-	Reference  *common.Reference // reference key for the transaction (optional, v1 only)
-	Memo       *[]byte           // memo for the transaction (optional, v1 only)
+	FeeTokenID uint16   // ERC20 token ID for fee payment (0 = ETH)
+	FeeLimit   *big.Int // maximum fee in token units (optional)
 
 	// Signature values
 	V *big.Int `json:"v" gencodec:"required"`
 	R *big.Int `json:"r" gencodec:"required"`
 	S *big.Int `json:"s" gencodec:"required"`
+
+	Version   uint8             // version of morph tx (0 = legacy, 1 = with reference/memo)
+	Reference *common.Reference // reference key for the transaction (optional, v1 only)
+	Memo      *[]byte           // memo for the transaction (optional, v1 only)
 }
 
 // morphTxV0RLP is the RLP encoding structure for MorphTx version 0 (legacy format)
-type morphTxV0RLP struct {
+type v0MorphTxRLP struct {
 	ChainID    *big.Int
 	Nonce      uint64
 	GasTipCap  *big.Int
@@ -74,7 +77,7 @@ type morphTxV0RLP struct {
 }
 
 // morphTxV1RLP is the RLP encoding structure for MorphTx version 1 (with Reference/Memo)
-type morphTxV1RLP struct {
+type v1MorphTxRLP struct {
 	ChainID    *big.Int
 	Nonce      uint64
 	GasTipCap  *big.Int
@@ -84,14 +87,14 @@ type morphTxV1RLP struct {
 	Value      *big.Int
 	Data       []byte
 	AccessList AccessList
-	Version    uint8
 	FeeTokenID uint16
 	FeeLimit   *big.Int
-	Reference  *common.Reference
-	Memo       *[]byte
 	V          *big.Int
 	R          *big.Int
 	S          *big.Int
+	Version    uint8
+	Reference  *common.Reference
+	Memo       *[]byte
 }
 
 // copy creates a deep copy of the transaction data and initializes all fields.
@@ -101,10 +104,10 @@ func (tx *MorphTx) copy() TxData {
 		Gas:        tx.Gas,
 		To:         copyAddressPtr(tx.To),
 		Data:       common.CopyBytes(tx.Data),
+		FeeTokenID: tx.FeeTokenID,
+		Version:    tx.Version,
 		Reference:  copyReferencePtr(tx.Reference),
 		Memo:       copyBytesPtr(tx.Memo),
-		Version:    tx.Version,
-		FeeTokenID: tx.FeeTokenID,
 		// These are copied below.
 		AccessList: make(AccessList, len(tx.AccessList)),
 		FeeLimit:   new(big.Int),
@@ -179,7 +182,7 @@ func (tx *MorphTx) setSignatureValues(chainID, v, r, s *big.Int) {
 func (tx *MorphTx) encode(b *bytes.Buffer) error {
 	if tx.Version == MorphTxVersion0 {
 		// Encode as v0 format (legacy)
-		return rlp.Encode(b, &morphTxV0RLP{
+		return rlp.Encode(b, &v0MorphTxRLP{
 			ChainID:    tx.ChainID,
 			Nonce:      tx.Nonce,
 			GasTipCap:  tx.GasTipCap,
@@ -197,7 +200,7 @@ func (tx *MorphTx) encode(b *bytes.Buffer) error {
 		})
 	}
 	// Encode as v1 format (with Version, Reference, Memo)
-	return rlp.Encode(b, &morphTxV1RLP{
+	return rlp.Encode(b, &v1MorphTxRLP{
 		ChainID:    tx.ChainID,
 		Nonce:      tx.Nonce,
 		GasTipCap:  tx.GasTipCap,
@@ -207,46 +210,68 @@ func (tx *MorphTx) encode(b *bytes.Buffer) error {
 		Value:      tx.Value,
 		Data:       tx.Data,
 		AccessList: tx.AccessList,
-		Version:    tx.Version,
 		FeeTokenID: tx.FeeTokenID,
 		FeeLimit:   tx.FeeLimit,
-		Reference:  tx.Reference,
-		Memo:       tx.Memo,
 		V:          tx.V,
 		R:          tx.R,
 		S:          tx.S,
+		Version:    tx.Version,
+		Reference:  tx.Reference,
+		Memo:       tx.Memo,
 	})
 }
 
 func (tx *MorphTx) decode(input []byte) error {
-	// Try to decode as v1 format first
-	var v1 morphTxV1RLP
-	if err := rlp.DecodeBytes(input, &v1); err == nil && v1.Version > 0 {
-		tx.ChainID = v1.ChainID
-		tx.Nonce = v1.Nonce
-		tx.GasTipCap = v1.GasTipCap
-		tx.GasFeeCap = v1.GasFeeCap
-		tx.Gas = v1.Gas
-		tx.To = v1.To
-		tx.Value = v1.Value
-		tx.Data = v1.Data
-		tx.AccessList = v1.AccessList
-		tx.Version = v1.Version
-		tx.FeeTokenID = v1.FeeTokenID
-		tx.FeeLimit = v1.FeeLimit
-		tx.Reference = v1.Reference
-		tx.Memo = v1.Memo
-		tx.V = v1.V
-		tx.R = v1.R
-		tx.S = v1.S
+	if err := decodeV1MorphTxRLP(tx, input); err == nil {
 		return nil
 	}
+	if err := decodeV0MorphTxRLP(tx, input); err == nil {
+		return nil
+	}
+	return errors.New("failed to decode morph tx")
+}
 
-	// Fall back to v0 format (legacy)
-	var v0 morphTxV0RLP
-	if err := rlp.DecodeBytes(input, &v0); err != nil {
+func decodeV1MorphTxRLP(tx *MorphTx, blob []byte) error {
+	var v1 v1MorphTxRLP
+	if err := rlp.DecodeBytes(blob, &v1); err != nil {
 		return err
 	}
+
+	if v1.Version != MorphTxVersion1 {
+		return errors.New("invalid morph tx version, expected 1, got " + strconv.Itoa(int(v1.Version)))
+	}
+
+	tx.ChainID = v1.ChainID
+	tx.Nonce = v1.Nonce
+	tx.GasTipCap = v1.GasTipCap
+	tx.GasFeeCap = v1.GasFeeCap
+	tx.Gas = v1.Gas
+	tx.To = v1.To
+	tx.Value = v1.Value
+	tx.Data = v1.Data
+	tx.AccessList = v1.AccessList
+	tx.Version = v1.Version
+	tx.FeeTokenID = v1.FeeTokenID
+	tx.FeeLimit = v1.FeeLimit
+	tx.Reference = v1.Reference
+	tx.Memo = v1.Memo
+	tx.V = v1.V
+	tx.R = v1.R
+	tx.S = v1.S
+
+	return nil
+}
+
+func decodeV0MorphTxRLP(tx *MorphTx, blob []byte) error {
+	var v0 v0MorphTxRLP
+	if err := rlp.DecodeBytes(blob, &v0); err != nil {
+		return err
+	}
+
+	if v0.FeeTokenID == 0 {
+		return errors.New("invalid fee token id, expected non-zero")
+	}
+
 	tx.ChainID = v0.ChainID
 	tx.Nonce = v0.Nonce
 	tx.GasTipCap = v0.GasTipCap
@@ -256,75 +281,57 @@ func (tx *MorphTx) decode(input []byte) error {
 	tx.Value = v0.Value
 	tx.Data = v0.Data
 	tx.AccessList = v0.AccessList
-	tx.Version = MorphTxVersion0
 	tx.FeeTokenID = v0.FeeTokenID
 	tx.FeeLimit = v0.FeeLimit
-	tx.Reference = nil
-	tx.Memo = nil
 	tx.V = v0.V
 	tx.R = v0.R
 	tx.S = v0.S
+
 	return nil
 }
 
 func (tx *MorphTx) sigHash(chainID *big.Int) common.Hash {
-	switch tx.Version {
-	case MorphTxVersion0:
-		// v0 sigHash (legacy format without Version, Reference, Memo)
-		return prefixedRlpHash(
-			MorphTxType,
-			[]any{
-				chainID,
-				tx.Nonce,
-				tx.GasTipCap,
-				tx.GasFeeCap,
-				tx.Gas,
-				tx.To,
-				tx.Value,
-				tx.Data,
-				tx.AccessList,
-				tx.FeeTokenID,
-				tx.FeeLimit,
-			})
-	case MorphTxVersion1:
-		// v1 sigHash (with Version, Reference, Memo)
-		return prefixedRlpHash(
-			MorphTxType,
-			[]any{
-				chainID,
-				tx.Nonce,
-				tx.GasTipCap,
-				tx.GasFeeCap,
-				tx.Gas,
-				tx.To,
-				tx.Value,
-				tx.Data,
-				tx.AccessList,
-				tx.Version,
-				tx.FeeTokenID,
-				tx.FeeLimit,
-				tx.Reference,
-				tx.Memo,
-			})
-	default:
-		// v1 sigHash (with Version, Reference, Memo)
-		return prefixedRlpHash(
-			MorphTxType,
-			[]any{
-				chainID,
-				tx.Nonce,
-				tx.GasTipCap,
-				tx.GasFeeCap,
-				tx.Gas,
-				tx.To,
-				tx.Value,
-				tx.Data,
-				tx.AccessList,
-				tx.Version,
-				tx.FeeTokenID,
-				tx.FeeLimit,
-				tx.Reference,
-				tx.Memo,
-			})
+	if tx.Version == MorphTxVersion0 {
+		return tx.v0SigHash(chainID)
 	}
+	return tx.v1SigHash(chainID)
+}
+
+func (tx *MorphTx) v1SigHash(chainID *big.Int) common.Hash {
+	return prefixedRlpHash(
+		MorphTxType,
+		[]any{
+			chainID,
+			tx.Nonce,
+			tx.GasTipCap,
+			tx.GasFeeCap,
+			tx.Gas,
+			tx.To,
+			tx.Value,
+			tx.Data,
+			tx.AccessList,
+			tx.FeeTokenID,
+			tx.FeeLimit,
+			tx.Version,
+			tx.Reference,
+			tx.Memo,
+		})
+}
+
+func (tx *MorphTx) v0SigHash(chainID *big.Int) common.Hash {
+	return prefixedRlpHash(
+		MorphTxType,
+		[]any{
+			chainID,
+			tx.Nonce,
+			tx.GasTipCap,
+			tx.GasFeeCap,
+			tx.Gas,
+			tx.To,
+			tx.Value,
+			tx.Data,
+			tx.AccessList,
+			tx.FeeTokenID,
+			tx.FeeLimit,
+		})
 }
